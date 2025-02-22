f15eed1e6c058cdb74b84ed0e3a99bf8e351aa45
Jagadesh-31 <jjagadesh980@gmail.com>
2025-02-22 13:12:33 +0530

    Update userRouter.js

--- userRouter.js
+++ userRouter.js
@@ -1,5 +1,5 @@
 let express = require('express');
-let { addUser, findUser, updateUser, deleteUser } = require('/db.js');  
+let { addUser, findUser, updateUser, deleteUser } = require('./db.js');  
 
 let dbroutes = express.Router();
 dbroutes.post('/addUser', addUser);
