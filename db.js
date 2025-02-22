3f694011448bb6d0239eee74622587afc3450f61
Jagadesh-31 <jjagadesh980@gmail.com>
2025-02-22 19:21:21 +0530

    Update db.js

--- db.js
+++ db.js
@@ -15,38 +15,19 @@ let addUser = async (req, res) => {
 };
 
 let findUser = async (req, res) => {
-    let userId = req.params.id;
+    let username = req.params.username;
+    console.log(`Finding user with username: ${username}`); // Add this line for logging
     try {
-        let findRes = await userModel.findOne({ _id: userId });
+        let findRes = await userModel.findOne({ username : username });
+        if (!findRes) {
+            return res.status(404).json({ error: "User not found" });
+        }
         res.status(200).json(findRes);
     } catch (error) {
+        console.error(`Error finding user: ${error.message}`); // Add this line for logging
         res.status(500).json({ error: error.message });
     }
 };
 
-let updateUser = async (req, res) => {
-    let userId = req.params.id;
-    let user = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    };
-    try {
-        let updateRes = await userModel.updateOne({ _id: userId }, user);
-        res.status(200).json(updateRes); 
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
 
-let deleteUser = async (req, res) => {
-    let userId = req.params.id;
-    try {
-        let deleteRes = await userModel.deleteOne({ _id: userId });
-        res.status(200).json(deleteRes); 
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-module.exports = { addUser, findUser, updateUser, deleteUser };
+module.exports = { addUser, findUser};
